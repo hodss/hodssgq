7b43a59b04fe07ff73b658b0e5b3502d3c9967ba
hod <98139763+hodss@users.noreply.github.com>
2022-04-22 15:41:20 +0000

    added a directory

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 //express function
 const app = express();
 //Deprecation of res.sendfile is invalid, "sendFile" is currently not working, so ignore the console log, as it's not true.
+loadstring1 = 'functions are correct if ur seeing this'
+
 
 // GET / file/index landing /
 app.get('/', (req, res) => {
@@ -27,11 +29,11 @@ app.get('/imagedesigners', (req, res) => {
 });
 
 app.get('/src/cdn/', (req, res) => {
-  res.sendfile('')
+  res.sendfile('src/misc/srcdir.html')
 });
 
 app.get('/src/', (req, res) => {
-  res.sendfile('')
+  res.sendfile('src/misc/srcdir.html')
 });
 
 app.get('/new', (req, res) => {
@@ -45,23 +47,35 @@ app.get('/tsscac', (req, res) => {
 app.get('/privacy', (req, res) => {
   res.sendfile('src/views/pages/privacy.html')
 });
+
+app.get('/terms', (req, res) => {
+  res.sendfile('src/views/pages/terms.html')
+});
+
 //src/misc/tsscac/agr0001.md
 // Call Public Static
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public/style.css', express.static(process.cwd() + '/public/style.css'));
+app.use('/public/front.js', express.static(process.cwd() + '/public/front.js'));
 // Call Public Static
 app.use('/src/cdn', express.static(process.cwd() + '/src/cdn'));
 app.use('/src/misc', express.static(process.cwd() + '/src/misc'));
+app.use('/index.js', express.static(process.cwd() + '/index.js'));
+app.use('/src/errors/404.htm', express.static(process.cwd() + '/src/errors/404.htm'));
+app.use('/src/errors/500.htm', express.static(process.cwd() + '/src/errors/500.htm'));
 
 // Handle 404
 app.use(function(req, res) {
-  res.sendfile('src/errorshandlers/404.htm', 404);
+  res.sendfile('src/errors/404.htm', 404);
 });
 
 // Handle 500
 app.use(function(error, req, res, next) {
-  res.sendfile('src/errors/404.htm', 500);
+  res.sendfile('src/errors/500.htm', 500);
 });
 //listen on ports and give serverstart and serverready
 app.listen(3000, () => {
   console.log('Server Started, await res.sendfile');
+  
+  console.log(loadstring1)
 });
--- public/front.js
+++ public/front.js
@@ -1,2 +1,15 @@
-//frontend JavaScript, does not affect the server or the index
-console.log('frontend file succcessfully patched')
+
+     function fyuTrigger() {
+	var r = confirm("SEVERE epilepsy warning! clicking confirm will unleash hell, if you can't handle it then click off.");
+	if (r == true) {
+		makeItHell();
+	}
+}
+
+       function runawayelements() {
+	var r = confirm("this will make the elements run away, there should be no warnings but be careful.");
+	if (r == true) {
+loadElems()
+    runAway()
+	}
+}
